7a6fb33225197a8257e60401838b59bc870e6330
Lakjeewa Wijebandara <lakjeewa.wijebandara@gmail.com>
2024-10-17 15:09:06 +0000

    Added : header for api

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,5 +1,22 @@
+import { useEffect } from "react";
+
 const Home = () => {
-    return <h1>Home</h1>;
+
+  useEffect(()=>{
+    //fetch all certificates per user
+    
+  },[]);
+    return <div>
+      <div id="header">
+          <button>Add New Certificate</button>
+      </div>
+      <div id="content">
+            <h1>category</h1>
+            <h2>Name of Certificate</h2>
+            <p>Description</p>
+            <a>File</a>
+      </div>
+    </div>;
   };
   
   export default Home;
