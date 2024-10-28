1665448918e2b07ff6948c875fb004dbc48209d4
Lakjeewa Wijebandara <lakjeewa.wijebandara@gmail.com>
2024-10-28 22:22:58 +0000

    Integrated : certificate update

--- src/Certificate.tsx
+++ src/Certificate.tsx
@@ -5,32 +5,55 @@ import { useForm } from "react-hook-form";
 import * as yup from "yup";
 import { useEffect, useState } from "react";
 import Loader from "./Loader";
-import { useNavigate } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
+import { format } from "date-fns";
 
-const schema = yup.object({
-    name: yup.string().required(),
-    category: yup.string().required(),
-    description: yup.string().required(),
-    dateAwarded: yup.date().typeError("Invalid date format").required("Certificate Awarded Date is required"),
-    uploaded_file: yup.mixed().required("A file is required").test("fileType", "A file is required",
-        (value: any) => {
-            return value && value.length > 0;
-        })
-}).required();
 
-type FormData = yup.InferType<typeof schema>;
 
 export default function Certificate() {
     const navigate = useNavigate();
-    const { register, handleSubmit, formState: { errors } } = useForm<FormData>({
-        resolver: yupResolver(schema)
-    });
+    const location = useLocation();
+    const updateData = location.state;
     const [isLoading, setIsLoading] = useState(false);
     const [categories, setCategories] = useState([] as any);
+    console.log("updateData ", updateData);
+
+    const schema = yup.object({
+        name: yup.string().required(),
+        category: yup.string().required(),
+        description: yup.string().required(),
+        dateAwarded: yup.date().typeError("Invalid date format").required("Certificate Awarded Date is required"),
+        uploaded_file: updateData?.fileName ? yup.mixed().optional() : yup.mixed().required("A file is required").test("fileType", "A file is required",
+            (value: any) => {
+                return value && value.length > 0;
+            })
+    });
+
+    type FormData = yup.InferType<typeof schema>;
+
+    const { register, handleSubmit, formState: { errors }, reset } = useForm<FormData>({
+        resolver: yupResolver(schema),
+        defaultValues: updateData?.fileName ? {
+            name: updateData.name,
+            category: categories.indexOf(updateData.category),
+            description: updateData.description,
+            dateAwarded: format(updateData.dateAwarded, 'yyyy-MM-dd'),
+            uploaded_file: []
+        } as any : {}
+    });
+
 
     useEffect(() => {
         getCategories();
     }, [])
+
+    useEffect(() => {
+        if (updateData?.fileName)
+            reset({
+                category: categories.indexOf(updateData.category),
+            });
+    }, [categories])
+
 
     const onSubmit = async (data: FormData) => {
         console.log(data, (data.uploaded_file as any)[0]);
@@ -38,7 +61,7 @@ export default function Certificate() {
         const formData = new FormData();
 
         // Append other fields to the form data
-        formData.append("category", data.category);
+        formData.append("category", categories[data.category]);
         formData.append("dateAwarded", data.dateAwarded.toISOString());
         formData.append("description", data.description);
         formData.append("name", data.name);
@@ -46,8 +69,15 @@ export default function Certificate() {
         formData.append("uploaded_file", (data.uploaded_file as any)[0]);
 
         try {
-            await API.post(`certificate/add`, formData);
-            toast.success("Certificate Creation Successful!")
+            if (updateData?.fileName) {
+                formData.append("certificateId", updateData.certificateId);
+                formData.append("fileName", updateData.fileName);
+                await API.put(`certificate/update`, formData);
+                toast.success("Certificate Update Successful!")
+            } else {
+                await API.post(`certificate/add`, formData);
+                toast.success("Certificate Creation Successful!")
+            }
             setIsLoading(false);
             setTimeout(() => {
                 navigate("/home");
@@ -78,10 +108,10 @@ export default function Certificate() {
                 <div
                     className="absolute inset-0 bg-gradient-to-r from-blue-300 to-blue-600 shadow-lg transform -skew-y-6 sm:skew-y-0 sm:-rotate-6 sm:rounded-3xl">
                 </div>
-                <div className="relative px-4 py-10 bg-white shadow-lg sm:rounded-3xl sm:p-20">
+                <div className="relative px-4 py-10 bg-white shadow-lg sm:rounded-3xl sm:p-20" style={{ width: 580 }}>
                     <div className="max-w-md mx-auto">
                         <div>
-                            <h1 className="text-2xl font-semibold">Want to add new certificate? Fill And Submit!</h1>
+                            <h1 className="text-2xl font-semibold">{updateData?.fileUrl ? 'EDIT CERTIFICATE!' : 'ADD NEW CERTIFICATE'}</h1>
                         </div>
                         <div className="divide-y divide-gray-200">
                             <form onSubmit={handleSubmit(onSubmit)} className="py-8 text-base leading-6 space-y-4 text-gray-700 sm:text-lg sm:leading-7">
@@ -91,16 +121,16 @@ export default function Certificate() {
                                     <p className="text-red-500 text-sm">{errors.name?.message}</p>
                                 </div>
                                 <div className="relative" style={{ marginTop: 30 }}>
-                                    <select {...register("category")} autoComplete="off" id="category" name="category" className="peer placeholder-transparent h-10 w-full border-b-2 border-gray-300 text-gray-900 focus:outline-none focus:borer-rose-600">
-                                        {categories.map((cat: any) =>
-                                            <option key={cat} value={cat}>{cat}</option>
+                                    <select {...register("category")} id="category" name="category" className="peer placeholder-transparent h-10 w-full border-b-2 border-gray-300 text-gray-900 focus:outline-none focus:borer-rose-600">
+                                        {categories.map((cat: any, index: number) =>
+                                            <option key={cat} value={index}>{cat}</option>
                                         )}
                                     </select>
                                     <label htmlFor="category" className="absolute left-0 -top-3.5 text-gray-600 text-base peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-440 peer-placeholder-shown:top-2 transition-all peer-focus:-top-3.5 peer-focus:text-gray-600 peer-focus:text-sm">Certificate Category</label>
                                     <p className="text-red-500 text-sm">{errors.category?.message}</p>
                                 </div>
-                                <div className="relative">
-                                    <input {...register("description")} autoComplete="off" id="description" name="description" type="text" className="peer placeholder-transparent h-10 w-full border-b-2 border-gray-300 text-gray-900 focus:outline-none focus:borer-rose-600" placeholder="Certificate Description" />
+                                <div className="relative" style={{ marginTop: 30 }}>
+                                    <textarea rows={5} {...register("description")} autoComplete="off" id="description" name="description" className="h-full peer placeholder-transparent h-10 w-full border-b-2 border-gray-300 text-gray-900 focus:outline-none focus:borer-rose-600" placeholder="Certificate Description" />
                                     <label htmlFor="description" className="absolute left-0 -top-3.5 text-gray-600 text-sm peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-440 peer-placeholder-shown:top-2 transition-all peer-focus:-top-3.5 peer-focus:text-gray-600 peer-focus:text-sm">Certificate Description</label>
                                     <p className="text-red-500 text-sm">{errors.description?.message}</p>
                                 </div>
@@ -112,6 +142,7 @@ export default function Certificate() {
                                     <p className="text-red-500 text-sm">{errors.dateAwarded?.message}</p>
                                 </div>
                                 <div className="relative" style={{ marginTop: 30 }}>
+                                    {updateData?.fileUrl ? <div className="text-left"><a className="ml-auto text-base font-medium text-blue-500" target="_blank" href={updateData.fileUrl}>View File</a></div> : ""}
                                     <input {...register("uploaded_file")} accept="application/pdf" autoComplete="off" id="uploaded_file" name="uploaded_file" type="file" className="mt-4 peer placeholder-transparent h-10 w-full border-b-2 border-gray-300 text-gray-900 focus:outline-none focus:borer-rose-600" placeholder="Certificate File" />
                                     <label htmlFor="uploaded_file" className="absolute left-0 -top-3.5 text-gray-600 text-sm peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-440 peer-placeholder-shown:top-2 transition-all peer-focus:-top-3.5 peer-focus:text-gray-600 peer-focus:text-sm">Certificate File</label>
                                     <p className="text-red-500 text-sm">{errors.uploaded_file?.message}</p>
--- src/Home.tsx
+++ src/Home.tsx
@@ -62,36 +62,43 @@ const Home = () => {
       </div>
     </div>
     <div id="content">
-      {Object.keys(certs).map((key: any) =>
-        <div key={key} className="pb-4">
+      {Object.keys(certs).length === 0 ?
+        <div className="p-28">
           <h1 className="text-4xl md:text-5xl lg:text-6xl font-bold text-blue-500">
-            {key}
+            No certificates to show 😞, start by adding a new certificate.
           </h1>
-          {certs[key].map((certData: any, index: number) =>
-            <div key={index}>
-              <div className="mx-auto mt-11 w-96 transform overflow-hidden rounded-lg bg-white dark:bg-slate-800 shadow-md duration-300 hover:scale-105 hover:shadow-lg">
-                <div className="p-4">
-                  <h2 className="mb-2 text-xl font-medium dark:text-white text-gray-900 underline">{certData.name}</h2>
-                  <p className="mb-2 text-base dark:text-gray-300 text-gray-700 text-left">{certData.description}</p>
-                  <p className="mb-2 text-base dark:text-gray-300 text-gray-700 text-left font-semibold">{'Awarded at :  ' + format(new Date(certData.dateAwarded), "yyyy/MM/dd")}</p>
-                  <div className="flex items-center justify-between">
-                    <button className="ml-auto text-base font-medium text-red-500"
-                      onClick={() => { deleteCert(certData) }}
-                    >Delete</button>
+        </div>
+        :
+        Object.keys(certs).map((key: any) =>
+          <div key={key} className="pb-4">
+            <h1 className="text-4xl md:text-5xl lg:text-6xl font-bold text-blue-500">
+              {key}
+            </h1>
+            {certs[key].map((certData: any, index: number) =>
+              <div key={index}>
+                <div className="mx-auto mt-11 w-96 transform overflow-hidden rounded-lg bg-white dark:bg-slate-800 shadow-md duration-300 hover:scale-105 hover:shadow-lg">
+                  <div className="p-4">
+                    <h2 className="mb-2 text-xl font-medium dark:text-white text-gray-900 underline">{certData.name}</h2>
+                    <p className="mb-2 text-base dark:text-gray-300 text-gray-700 text-left">{certData.description}</p>
+                    <p className="mb-2 text-base dark:text-gray-300 text-gray-700 text-left font-semibold">{'Awarded at :  ' + format(new Date(certData.dateAwarded), "yyyy/MM/dd")}</p>
+                    <div className="flex items-center justify-between">
+                      <button className="ml-auto text-base font-medium text-red-500"
+                        onClick={() => { deleteCert(certData) }}
+                      >Delete</button>
 
-                    <a className="ml-auto text-base font-medium text-blue-500" href={certData.fileUrl}>Edit</a>
+                      <NavLink to="/certificate" state={certData} className="ml-auto text-base font-medium text-blue-500" >Edit</NavLink>
 
-                    <a className="ml-auto text-base font-medium text-blue-500" target="_blank" href={certData.fileUrl}>View File</a>
+                      <a className="ml-auto text-base font-medium text-blue-500" target="_blank" href={certData.fileUrl}>View File</a>
+                    </div>
                   </div>
                 </div>
               </div>
-            </div>
-          )
+            )
 
-          }
+            }
 
-        </div>
-      )
+          </div>
+        )
 
       }
 
